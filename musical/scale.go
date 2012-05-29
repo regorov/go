d871e7f0efaf908e2d33a1910a56abf337213d77
Kier Davis <kierdavis@gmail.com>
2012-05-29 22:41:57 +0100

    Auto commit 29/05/2012 22:41:56

--- musical/scale.go
+++ musical/scale.go
@@ -1,86 +1,86 @@
 package musical
 
 var (
-    Major           = []uint8{2, 2, 1, 2, 2, 2, 1}
-    Minor           = []uint8{2, 1, 2, 2, 1, 2, 2}
-    MelodicMinor    = []uint8{2, 1, 2, 2, 2, 2, 1}
-    HarmonicMinor   = []uint8{2, 1, 2, 2, 1, 3, 1}
-    PentatonicMajor = []uint8{2, 2, 3, 2, 3}
-    BluesMajor      = []uint8{3, 2, 1, 1, 2, 3}
-    PentatonicMinor = []uint8{3, 2, 2, 3, 2}
-    BluesMinor      = []uint8{3, 2, 1, 1, 3, 2}
-    Augmented       = []uint8{3, 1, 3, 1, 3, 1}
-    Diminished      = []uint8{2, 1, 2, 1, 2, 1, 2, 1}
-    Chromatic       = []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-    WholeHalf       = []uint8{2, 1, 2, 1, 2, 1, 2, 1}
-    HalfWhole       = []uint8{1, 2, 1, 2, 1, 2, 1, 2}
-    WholeTone       = []uint8{2, 2, 2, 2, 2, 2}
-    AugmentedFifth  = []uint8{2, 2, 1, 2, 1, 1, 2, 1}
-    Japanese        = []uint8{1, 4, 2, 1, 4}
-    Oriental        = []uint8{1, 3, 1, 1, 3, 1, 2}
-    Ionian          = []uint8{2, 2, 1, 2, 2, 2, 1}
-    Dorian          = []uint8{2, 1, 2, 2, 2, 1, 2}
-    Phrygian        = []uint8{1, 2, 2, 2, 1, 2, 2}
-    Lydian          = []uint8{2, 2, 2, 1, 2, 2, 1}
-    Mixolydian      = []uint8{2, 2, 1, 2, 2, 1, 2}
-    Aeolian         = []uint8{2, 1, 2, 2, 1, 2, 2}
-    Locrian         = []uint8{1, 2, 2, 1, 2, 2, 2}
+	Major		= []uint8{2, 2, 1, 2, 2, 2, 1}
+	Minor		= []uint8{2, 1, 2, 2, 1, 2, 2}
+	MelodicMinor	= []uint8{2, 1, 2, 2, 2, 2, 1}
+	HarmonicMinor	= []uint8{2, 1, 2, 2, 1, 3, 1}
+	PentatonicMajor	= []uint8{2, 2, 3, 2, 3}
+	BluesMajor	= []uint8{3, 2, 1, 1, 2, 3}
+	PentatonicMinor	= []uint8{3, 2, 2, 3, 2}
+	BluesMinor	= []uint8{3, 2, 1, 1, 3, 2}
+	Augmented	= []uint8{3, 1, 3, 1, 3, 1}
+	Diminished	= []uint8{2, 1, 2, 1, 2, 1, 2, 1}
+	Chromatic	= []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	WholeHalf	= []uint8{2, 1, 2, 1, 2, 1, 2, 1}
+	HalfWhole	= []uint8{1, 2, 1, 2, 1, 2, 1, 2}
+	WholeTone	= []uint8{2, 2, 2, 2, 2, 2}
+	AugmentedFifth	= []uint8{2, 2, 1, 2, 1, 1, 2, 1}
+	Japanese	= []uint8{1, 4, 2, 1, 4}
+	Oriental	= []uint8{1, 3, 1, 1, 3, 1, 2}
+	Ionian		= []uint8{2, 2, 1, 2, 2, 2, 1}
+	Dorian		= []uint8{2, 1, 2, 2, 2, 1, 2}
+	Phrygian	= []uint8{1, 2, 2, 2, 1, 2, 2}
+	Lydian		= []uint8{2, 2, 2, 1, 2, 2, 1}
+	Mixolydian	= []uint8{2, 2, 1, 2, 2, 1, 2}
+	Aeolian		= []uint8{2, 1, 2, 2, 1, 2, 2}
+	Locrian		= []uint8{1, 2, 2, 1, 2, 2, 2}
 )
 
 type Scale struct {
-    Root      Note
-    Intervals []uint8
+	Root		Note
+	Intervals	[]uint8
 }
 
 func NewScale(root Note, intervals []uint8) (scale Scale) {
-    return Scale{
-        Root:      root.AtOctave(0),
-        Intervals: intervals,
-    }
+	return Scale{
+		Root:		root.AtOctave(0),
+		Intervals:	intervals,
+	}
 }
 
 func (scale Scale) Get(index int) (note Note) {
-    intervals := scale.Intervals
-    note = scale.Root
+	intervals := scale.Intervals
+	note = scale.Root
 
-    if index > 0 {
-        x := 0
+	if index > 0 {
+		x := 0
 
-        for i := 0; i < index; i++ {
-            note = note.Transpose(int(intervals[x]))
-            x = (x + 1) % len(intervals)
-        }
+		for i := 0; i < index; i++ {
+			note = note.Transpose(int(intervals[x]))
+			x = (x + 1) % len(intervals)
+		}
 
-    } else {
-        x := 0
+	} else {
+		x := 0
 
-        for i := 0; i < index; i++ {
-            x = (x - 1) % len(intervals)
-            note = note.Transpose(-int(intervals[x]))
-        }
-    }
+		for i := 0; i < index; i++ {
+			x = (x - 1) % len(intervals)
+			note = note.Transpose(-int(intervals[x]))
+		}
+	}
 
-    return note
+	return note
 }
 
 func (scale Scale) Index(note Note) (index int) {
-    intervals := scale.Intervals
-    index = 0
-    x := scale.Root
-    i := 0
+	intervals := scale.Intervals
+	index = 0
+	x := scale.Root
+	i := 0
 
-    for x < note {
-        x = x.Transpose(int(intervals[i]))
-        i = (i + 1) % len(intervals)
-    }
+	for x < note {
+		x = x.Transpose(int(intervals[i]))
+		i = (i + 1) % len(intervals)
+	}
 
-    if x == note {
-        return index
-    }
+	if x == note {
+		return index
+	}
 
-    return -1
+	return -1
 }
 
 func (scale Scale) Transpose(note Note, interval int) (res Note) {
-    return scale.Get(scale.Index(note) + interval)
+	return scale.Get(scale.Index(note) + interval)
 }
--- musical/source.go
+++ musical/source.go
@@ -1,44 +1,62 @@
 package musical
 
 import (
-    "math"
+	"math"
 )
 
 type Chunk []float64
 
 func Silence(length float64, rate float64) (chunk Chunk) {
-    return make(Chunk, int(length*rate))
+	return make(Chunk, int(length*rate))
 }
 
 func generateWaveInput(freq float64, length float64, rate float64, phase float64) (chunk Chunk) {
-    ilength := int(length * rate)
-    factor := (freq * math.Pi * 2) / rate
-    phase *= rate / 2.0
+	ilength := int(length * rate)
+	factor := (freq * math.Pi * 2) / rate
+	phase *= rate / 2.0
 
-    chunk = make(Chunk, ilength)
-    for i := 0; i < ilength; i++ {
-        chunk[i] = (float64(i) + phase) * factor
-    }
+	chunk = make(Chunk, ilength)
+	for i := 0; i < ilength; i++ {
+		chunk[i] = (float64(i) + phase) * factor
+	}
 
-    return chunk
+	return chunk
 }
 
 func Sine(freq float64, length float64, rate float64, phase float64) (chunk Chunk) {
-    chunk = generateWaveInput(freq, length, rate, phase)
+	chunk = generateWaveInput(freq, length, rate, phase)
 
-    for i := 0; i < len(chunk); i++ {
-        chunk[i] = math.Sin(chunk[i])
-    }
+	for i := 0; i < len(chunk); i++ {
+		chunk[i] = math.Sin(chunk[i])
+	}
 
-    return chunk
+	return chunk
 }
 
 func Sawtooth(freq float64, length float64, rate float64, phase float64) (chunk Chunk) {
-    chunk = generateWaveInput(freq, length, rate, phase)
+	chunk = generateWaveInput(freq, length, rate, phase)
 
-    for i := 0; i < len(chunk); i++ {
-        chunk[i] = math.Mod(chunk[i], 1.0)
-    }
+	for i := 0; i < len(chunk); i++ {
+		tmod := math.Mod(chunk[i], 2*math.PI)
+		chunk[i] = (tmod / math.PI) - 1
+	}
 
-    return chunk
+	return chunk
+}
+
+func Square(freq float64, length float64, rate float64, phase float64) (chunk Chunk) {
+	chunk = generateWaveInput(freq, length, rate, phase)
+
+	for i := 0; i < len(chunk); i++ {
+		t := chunk[i]
+		tmod := math.Mod(chunk[i], 2*math.PI)
+
+		if tmod < pi {
+			chunk[i] = 1
+		} else {
+			chunk[i] = -1
+		}
+	}
+
+	return chunk
 }
